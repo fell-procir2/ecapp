4e37331100f454e483213ffb86646e13beb43141
fell <username0515@yahoo.co.jp>
2019-11-04 11:00:56 +0900

    public_to_root_by_.htaccess_20191104

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+//.\public\
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
-Auth::routes();
+Route::get('/hello', function() {
+	return 'Hello world';
+});
 
+//ログイン状態
+Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
