911030aca62ffb0ae36ef11db308b46ea99340ef
fell <username0515@yahoo.co.jp>
2019-11-14 17:22:06 +0900

    admin_item_complete_k34_20191122

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,27 +2,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//eloquent:Itemクラス宣言
-use App\Item;
+use App\Http\Requests\ItemRequest;
+use App\Item; //eloquent
 
 class ItemController extends Controller
 {
-    //商品一覧
-    public function index()
-    {
+    public function index() {
+		session(['id' => '']);
 		$items = Item::all();
 		return view('item.index', compact('items'));
-		//debug
-		//foreach ($items as $item) {
-		//	echo $item->name . '<br>';
-		//}
 	}
-	//商品詳細
 	public function detail($id) {
+		session(['id' => $id]);
 		$item = Item::find($id);
 		return view('item.detail', compact('item'));
 	}
-	public function role($role) {
-		echo $role;
+	public function edit(Request $request) {
+		$name = $request->input('name');
+		$content = $request->input('content');
+		$price = $request->input('price');
+		$quantity = $request->input('quantity');
+		return view('item.edit', compact('name', 'content', 'price', 'quantity'));
+	}
+	public function create(ItemRequest $request) {
+		//追加処理
+		$name = $request->input('name');
+		$content = $request->input('content');
+		$price = $request->input('price');
+		$quantity = $request->input('quantity');
+		$item = Item::create(compact('name', 'content', 'price', 'quantity'));
+		return redirect(route('item.index'))->with('message', '商品を追加しました。');
+	}
+	public function update(ItemRequest $request) {
+		$id = session('id');
+		//編集処理
+		$item = Item::findOrFail($id);
+		$item->fill(['name' => $request->input('name')]);
+		$item->fill(['content' => $request->input('content')]);
+		$item->fill(['quantity' => $request->input('quantity')]);
+		$item->save();
+		return redirect(route('item.detail', ['id' => $id]))->with('message', '内容を修正しました。');
 	}
 }
--- resources/views/item/detail.blade.php
+++ resources/views/item/detail.blade.php
@@ -1,16 +1,30 @@
-@section('title', '商品詳細')
-@section('body')
-<table>
-	<tr><th align="left" style="background-color:#e3f0fb">商品名:</th><td>{{ $item->name }}</td></tr>
-	<tr><th align="left" style="background-color:#e3f0fb">商品説明:</th><td>{{ $item->content }}</td></tr>
-	<tr><th align="left" style="background-color:#e3f0fb">価格:</th><td>{{ $item->price }}</td></tr>
-	<tr><th align="left" style="background-color:#e3f0fb">在庫:</th>
+@extends('layouts.mini_app')
+@section('content')
+<body>
+	<h1>商品詳細</h1>
+	<table>
+		<tr><th align="left" style="background-color:#e3f0fb">商品名:</th><td>{{ $item->name }}</td></tr>
+		<tr><th align="left" style="background-color:#e3f0fb">商品説明:</th><td>{{ $item->content }}</td></tr>
+		<tr><th align="left" style="background-color:#e3f0fb">価格:</th><td>{{ $item->price }}</td></tr>
+		<tr><th align="left" style="background-color:#e3f0fb">在庫:</th>
 		@if (0 < $item->quantity)
 			<td>あり</td>
 		@else
 			<td style="background-color:#e0ffff">なし</td>
 		@endif
-	</tr>
-</table>
-<br>
-<a href={{ route('item') }}>商品一覧に戻る</a>
+		</tr>
+	</table>
+	@role('admin')
+		<br>
+		<form method="post" action="{{ route('item.edit') }}">
+			{{ csrf_field() }}
+			<input type="hidden" name="name" value="{{ $item->name }}">
+			<input type="hidden" name="content" value="{{ $item->content }}">
+			<input type="hidden" name="price" value="{{ $item->price }}">
+			<input type="hidden" name="quantity" value="{{ $item->quantity }}">
+			<button type="submit">商品の編集</button>
+		</form>
+	@endrole
+	<h2><a href="{{ route('item.index') }}">商品一覧へ戻る</a></h2>
+</body>
+@endsection
